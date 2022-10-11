c43a22f8313c4fe5ab64544459ce286b1f6d354b
ValentinaZussino <vzussino@protonmail.com>
2022-10-11 17:44:28 +0200

    impostazione dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,3 +1,21 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
-alert('hello')
+
+// genero il numero casuale per il giocatore
+// genero il numero casuale per il pc
+// verifico il num maggiore
+// ricordarsi il pareggio
+
+let nPlayer = Math.floor(Math.random() * 6) + 1;
+console.log(nPlayer)
+
+let nComputer = Math.floor(Math.random() * 6) + 1;
+console.log(nComputer)
+
+if(nComputer > nPlayer){
+    document.getElementById('result').innerHTML = 'Ha vinto il computer'
+} else if(nPlayer > nComputer) {
+    document.getElementById('result').innerHTML = 'Hai vinto tu'
+} else {
+    document.getElementById('result').innerHTML = 'Avete pareggiato'
+}
