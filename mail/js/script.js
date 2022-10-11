6166087b06c7c39b04241759200ddfd55a207092
ValentinaZussino <vzussino@protonmail.com>
2022-10-11 17:13:13 +0200

    completamento mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,30 +1,33 @@
 // dichiaro variabile per campo input mail e button
 let mailInput = document.getElementById('mail');
-let btn = document.getElementById('v_btn')
+let searchBtn = document.getElementById('search-btn');
 
 // creo array con elenco mail
 const elencoEmails = ['federicorossi@gmail.com', 'valezussino@libero.it', 'verdimattia@libero.it', 'pincopallo@gmail.com'];
 
-// prendo il valore inserito nel campo email e lo cerco dentro l'array creando const
-const provaV = function(){
+// creo funzione per btn
+const search = function(){
     // prendo il value della mail
     let mailValue = mailInput.value;
-    console.log(mailValue)
     // creo un check per l'array
     let checkEmail = false;
-    // inizio a cercare
+    // creo ciclo
     for(let i = 0; i < elencoEmails.length; i++){
-    // prendo valori in array
-    const arrayValue = elencoEmails[i];
-    // verifico con check
-    if(mailValue == arrayValue){
+        // prendo valori in array
+        const arrayValue = elencoEmails[i];
+        // verifico con check
+        if(mailValue == arrayValue){
         checkEmail = true;
-    }}
+    }
+    }
     // messaggi
     if(checkEmail){
-        document.getElementById('testo').innerHTML = 'ci sei'
+        document.getElementById('testo').innerHTML = 'Complimenti, sei presente!'
     } else {
-        document.getElementById('testo').innerHTML = 'non ci sei'
+        document.getElementById('testo').innerHTML = 'Non sei presente nel nostro elenco purtroppo'
     }
+    // reset campo input mail
+    mailInput.value = '';
 }
-btn.addEventListener('click', provaV)
+// on click
+searchBtn.addEventListener('click', search)
