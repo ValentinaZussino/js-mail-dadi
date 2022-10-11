5ce739508dd898c8db1b23245190394065de85ac
ValentinaZussino <vzussino@protonmail.com>
2022-10-11 18:12:30 +0200

    messa a schermo + aggiunta btn

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,21 +1,22 @@
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
-// genero il numero casuale per il giocatore
-// genero il numero casuale per il pc
-// verifico il num maggiore
-// ricordarsi il pareggio
-
-let nPlayer = Math.floor(Math.random() * 6) + 1;
-console.log(nPlayer)
-
-let nComputer = Math.floor(Math.random() * 6) + 1;
-console.log(nComputer)
-
-if(nComputer > nPlayer){
-    document.getElementById('result').innerHTML = 'Ha vinto il computer'
-} else if(nPlayer > nComputer) {
-    document.getElementById('result').innerHTML = 'Hai vinto tu'
-} else {
-    document.getElementById('result').innerHTML = 'Avete pareggiato'
+// metto tutto in una funzione
+const play = function(){
+    // genero il numero casuale per il giocatore
+    let nPlayer = Math.floor(Math.random() * 6) + 1;
+    // metto a schermo nPlayer
+    document.getElementById('player-number').innerHTML = nPlayer;
+    // genero il numero casuale per il pc
+    let nComputer = Math.floor(Math.random() * 6) + 1;
+    // metto a schermo nComputer
+    document.getElementById('pc-number').innerHTML = nComputer;
+    // verifico il num maggiore e ricordarsi il pareggio
+    if(nComputer > nPlayer){
+        document.getElementById('result').innerHTML = 'Ha vinto il computer'
+    } else if(nPlayer > nComputer) {
+        document.getElementById('result').innerHTML = 'Hai vinto tu'
+    } else {
+        document.getElementById('result').innerHTML = 'Avete pareggiato'
+    }
 }
+// on click
+let btn = document.getElementById('play-btn');
+btn.addEventListener('click', play)
